92500fe2b7ab6d51840568dd13e21feda9111853
Faiz Ahmed <faaezahmd@gmail.com>
2022-11-09 13:20:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <VueJsonToTable :data="jsonData" />
+</template>
+
+<script>
+import { VueJsonToTable } from './components/';
+import json from "./data.json";
+
+export default {
+  name: "App",
+  components: {
+    VueJsonToTable
+  },
+  data() {
+    return {
+      jsonData: json
+    }
+  }
+}
+</script>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+export { default as VueJsonToTable } from './VueJsonToTable.vue';
+
+// Other components you plan to publish go here
